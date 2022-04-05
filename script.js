d0f49d92cf0d1595d9c406293097117626cb1cc9
pavanchambhare <pavanchambhare88gmail.com>
2022-04-05 17:34:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+    function mouseover()
+   {
+       document.getElementById("change"). src="1.png";
+   }
+    function mouseout()
+   {
+       document.getElementById("change"). src="2.png";
+   }
+   
